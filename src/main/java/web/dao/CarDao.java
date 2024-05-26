0b7c5aec8a132dd6962026b61153ecb825247a60
armgaloyan <armaa.galoyan@yandex.ru>
2024-05-26 20:49:38 +0300

    Задача 2.2.2. MVC-приложение.

---
+++ src/main/java/web/dao/CarDao.java
@@ -0,0 +1,24 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDao {
+    private List<Car> cars;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("BMW", "Black", 5));
+        cars.add(new Car("Mercedes", "White", 6));
+        cars.add(new Car("Audi", "Red", 7));
+        cars.add(new Car("Volkswagen", "Blue", 8));
+        cars.add(new Car("Haval", "White", 7));
+    }
+
+    public List<Car> getCars(int count) {
+        return cars.stream().limit(count).toList();
+    }
+}
