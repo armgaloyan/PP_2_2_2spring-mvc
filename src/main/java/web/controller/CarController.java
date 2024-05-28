4a772bd4cb72b11a185b0a7d0f8084c273f867be
armgaloyan <armaa.galoyan@yandex.ru>
2024-05-28 17:22:01 +0300

    Задача 2.2.2. MVC-приложение

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
     }
