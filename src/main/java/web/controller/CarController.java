dbf6da1310c81a84f36672e36ea1d007d565a2cd
armgaloyan <armaa.galoyan@yandex.ru>
2024-05-28 13:36:23 +0300

    Задача 2.2.2. MVC-приложение

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,5 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import web.service.CarServiceImpl;
 @Controller
 public class CarController {
     private final CarServiceImpl carService;
-    @Autowired
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,6 +1,5 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import web.dao.CarDao;
 import web.model.Car;
@@ -8,8 +7,12 @@ import web.model.Car;
 import java.util.List;
 @Component
 public class CarServiceImpl implements CarService {
-    @Autowired
-    private CarDao carDao;
+    private final CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
     @Override
     public List<Car> getCars(int count) {
         return carDao.getCars(count);
